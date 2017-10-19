ed327a9d1ea4d032aca7a86f0952659d2cb58fe5
root <root@localhost.localdomain>
2017-10-19 10:37:40 +0300

    after styling with eslint

--- App.js
+++ App.js
@@ -1,25 +1,35 @@
-
 import React from 'react';
-import { StyleSheet, Text, View, Component, AppRegistry, TouchableHighlight, Button, Image,ontouchend, button, Navigator } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Component,
+    AppRegistry,
+    TouchableHighlight,
+    Button,
+    Image,
+    ontouchend,
+    button,
+    Navigator
+} from 'react-native';
 
 import Login from './src/Login';
 //import Signup from './src/Signup';
 
 export default class App extends React.Component {
-
-  render() {
-    return (
-    <Login />
-    //<Signup />
-    );
-  }
+    render() {
+        return (
+            <Login />
+            //<Signup />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
