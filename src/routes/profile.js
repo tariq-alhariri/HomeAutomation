b593b7fdf26d0b92720ed5f91782d9da3df27369
kamellababidi <kamelallababidi@gmail.com>
2017-10-22 12:00:32 +0300

    signup and sign in in server

---
+++ src/routes/profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    Button,
+    KeyboardAvoidingView
+} from 'react-native';
+export default class Login extends React.Component {
+	constructor(props) {
+        super(props);
+    }
+    render(){
+    	<Text>hello profile</Text>
+    }
+}
